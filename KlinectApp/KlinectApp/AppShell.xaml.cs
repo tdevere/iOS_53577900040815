44d71e7fc77288e24b322814e222e848ea79757a
tdevere <tdevere@microsoft.com>
2021-08-26 18:21:11 -0500

    Add project files.

---
+++ KlinectApp/KlinectApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using KlinectApp.ViewModels;
+using KlinectApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace KlinectApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ KlinectApp/KlinectApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using KlinectApp.Models;
+using KlinectApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KlinectApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
